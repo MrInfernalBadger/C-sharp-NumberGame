b26224f9a5cf2df6d68f1da5e26b84bff925e65d
MrInfernalBadger <35194014+MrInfernalBadger@users.noreply.github.com>
2024-05-06 11:47:01 +1000

    Basic functionality added

--- NumberGame/Program.cs
+++ NumberGame/Program.cs
@@ -1,27 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberGame
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static Random rand = new Random();
+        private static int randomNumber;
+        static void Main()
         {
-
+            WelcomeScreen();
+            int guesses = RunGame();
+            Console.WriteLine($"Congratulation, you got it in {guesses} guess!");
+            Console.ReadLine();
         }
         static void WelcomeScreen()
         {
             Console.WriteLine("Welcome to the number guessing game!");
             Console.WriteLine("Press any key to continue");
         }
+        static int RunGame()
+        {
+            randomNumber = rand.Next(0, 1001);
+            int guess;
+            int numberOfGuesses = 0;
+
+            while (true)
+            {
+                guess = GetGuess();
+                numberOfGuesses += 1;
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    return numberOfGuesses;
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Too high!");
+                }
+            }
+        }
+        static int GetGuess()
+        {
+            int number;
+            while (true)
+            {
+                Console.WriteLine("Guess a number: ");
+                if (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+                else
+                {
+                    return number;
+                }
+                Console.Clear();
+            }
+        }
     }
 }
 
 /*
- 
+ Welcome user and begin game
+        TODO add difficulty level
+Prompt for guess
+Say if high or low
+        TODO say if way too high/low depending on difficulty
+        TODO number range greater on higher difficulties
+Congratulate on correct guess
+Ask if play again
 
 
 
