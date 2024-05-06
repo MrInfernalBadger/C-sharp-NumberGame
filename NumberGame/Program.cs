34288624d0ea44bb1ffe2e16e5d5dbc03aa35b31
MrInfernalBadger <35194014+MrInfernalBadger@users.noreply.github.com>
2024-05-06 10:24:33 +1000

    initialisation

---
+++ NumberGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void WelcomeScreen()
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("Press any key to continue");
+        }
+    }
+}
+
+/*
+ 
+
+
+
+ */
