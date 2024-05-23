63738dce2d752421c57968f53131a9a88c53d41e
MrInfernalBadger <35194014+MrInfernalBadger@users.noreply.github.com>
2024-05-23 13:38:53 +1000

    Improved communication

--- NumberGame/Program.cs
+++ NumberGame/Program.cs
@@ -4,14 +4,16 @@ namespace NumberGame
 {
     internal class Program
     {
-        private static  Random rand = new Random();
+        private static Random rand = new Random();
         private static int randomNumber;
+        private static int lowestClose = -1;
+        private static int highestClose = int.MaxValue;
 
         static void Main()
         {
             WelcomeScreen();
             RunGame(out int guesses);
-            Console.WriteLine($"Congratulations, you got it in {guesses} guesses!"); // Display different result messages
+            Console.WriteLine($"Congratulations, you got it in {guesses} guesses!");
             Console.ReadLine();
         }
 
@@ -19,60 +21,90 @@ namespace NumberGame
         {
             Console.WriteLine("Welcome to the number guessing game!");
             Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
         }
 
         static void RunGame(out int numberOfGuesses)
         {
+            //Console.Clear();
             randomNumber = rand.Next(0, 101);
-            Console.WriteLine(randomNumber);
             int guess = -1;
+
             numberOfGuesses = 0;
 
             while (guess != randomNumber)
             {
+                Console.Clear();
+                Console.WriteLine(randomNumber); // TESTING ONLY
+                DisplayPreviousGuesses();
                 guess = GetGuess();
                 numberOfGuesses += 1;
-                AssessGuess(guess);
+                TooHighTooLow(guess);
+
+                if (guess > lowestClose && guess < randomNumber)
+                {
+                    lowestClose = guess;
+                }
+
+                if (guess < highestClose && guess > randomNumber)
+                {
+                    highestClose = guess;
+                }
             }
         }
 
-        private static void AssessGuess(int guess)
+        private static void TooHighTooLow(int guess)
         {
-            if (guess == randomNumber)
-            {
-                Console.WriteLine("You guessed correctly!");
-            }
+            if (guess == -1){}
             else if (guess < randomNumber)
             {
-                Console.WriteLine("Too low!");
+                Console.WriteLine($"{guess} is too low!");
             }
             else if (guess > randomNumber)
             {
-                Console.WriteLine("Too high!");
+                Console.WriteLine($"{guess} is too high!");
             }
         }
 
         static int GetGuess()
         {
+            Console.WriteLine("Please enter a number: ");
             while (true)
             {
-                Console.WriteLine("Guess a number: ");
                 if (!int.TryParse(Console.ReadLine(), out int number))
                 {
+                    Console.Clear();
+                    DisplayPreviousGuesses();
                     Console.WriteLine("Please enter an integer.");
                 }
                 else
                 {
                     return number;
                 }
-                Console.Clear();
+
             }
 
+        }
+
+        static void DisplayPreviousGuesses()
+        {
+            bool lowestSet = lowestClose != -1;
+            bool highestSet = highestClose != int.MaxValue;
+
+            if (lowestSet && highestSet)
+            {
+                Console.WriteLine($"You know the number is between {lowestClose} and {highestClose}");
+            }
+            else if (lowestSet)
+            {
+                Console.WriteLine($"You know the number is greater than {lowestClose}");
+            }
+            else if (highestSet)
+            {
+                Console.WriteLine($"You know the number is less than {highestClose}");
+            }
         }
     }
 }
 
-/*
-TODO add difficulty level (greater range of numbers)
-TODO display different success message depending on guesses and difficulty
- */
+
